9d00355a05d75d5334a5341e9cfec99ec6a23e0b
Hasani Anthony Groce <hgroce@gmail.com>
2013-08-17 11:39:51 -0700

    all powerups have sound installed
    see above but shield, all bonues, and spike do not seem to play.

--- Mathius_Final/Assets/Components/Alien/Powerup Drops/Shield/Shield.cs
+++ Mathius_Final/Assets/Components/Alien/Powerup Drops/Shield/Shield.cs
@@ -5,7 +5,7 @@ public class Shield : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	SoundManager.SOUNDS.playSound(SoundManager.PU_SHIELD,CameraCollider.MATHIUS_EARTH_CAM);
 	}
 	
 	// Update is called once per frame
--- Mathius_Final/Assets/Components/Alien/Powerup Drops/Spike/Spike.cs
+++ Mathius_Final/Assets/Components/Alien/Powerup Drops/Spike/Spike.cs
@@ -5,7 +5,7 @@ public class Spike : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	SoundManager.SOUNDS.playSound(SoundManager.PU_SPIKE,CameraCollider.MATHIUS_EARTH_CAM);
 	}
 	
 	// Update is called once per frame
