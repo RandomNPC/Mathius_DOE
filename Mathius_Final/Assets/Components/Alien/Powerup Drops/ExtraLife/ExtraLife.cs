1257134019be1ca16bb0c52218800183ec4653ed
Hasani Anthony Groce <hgroce@gmail.com>
2013-08-17 00:09:24 -0700

    some powerups have sounds
    gold, silver life and bronze added to CS

--- Mathius_Final/Assets/Components/Alien/Powerup Drops/ExtraLife/ExtraLife.cs
+++ Mathius_Final/Assets/Components/Alien/Powerup Drops/ExtraLife/ExtraLife.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ExtraLife : MonoBehaviour {
 
 	void Start () {
+		SoundManager.SOUNDS.playSound(SoundManager.PU_LIFE,CameraCollider.MATHIUS_EARTH_CAM);
 		Mathius life = MasterController.BRAIN.m();
 		life.set_lives(life.get_lives()+1);
 	}
