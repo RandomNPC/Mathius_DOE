9c4cedb1bce3111d6d399e491029bb04afdf8a72
Paul Matias <bluedoctor7@aol.com>
2013-08-20 16:48:04 -0700

    GUI Manager II
    Now has toggle capabilities!
    -Each element can now be accessed by tags.
    -When a button's state has been changed, the OnToggle event gets
    triggered.
    -Added more Properties to GUIProperties
    -Included state change in ButtonName

--- Mathius_Final/Assets/Components/GUIs/GUIManager/GUIProperties.cs
+++ Mathius_Final/Assets/Components/GUIs/GUIManager/GUIProperties.cs
@@ -6,11 +6,15 @@ public class GUIProperties{
 	public Rect rect{get;private set;}
 	public GUIType type{get;private set;}
 	public string style{get;private set;}
+	public string name{get;private set;}
+	public bool check{get; set;}
 	
-	public GUIProperties(Rect rect, GUIType type,string style){
+	public GUIProperties(string name,Rect rect, GUIType type,string style, bool check){
+		this.name = name;
 		this.rect = rect;
 		this.type = type;
 		this.style = style;
+		this.check = check;
 	}
 	
 	
