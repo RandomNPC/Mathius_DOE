30d5dd5c93470d5824a2ff1a29a4744240733814
RandomNPC <bluedoctor7@aol.com>
2013-08-22 16:33:14 -0700

    Updated usage documentation of using GUIManager.
    Added the slider example.

--- MyGUI.cs
+++ MyGUI.cs
@@ -15,26 +15,34 @@ public class MyGUI : MonoBehaviour {
 		//Register the events this way. You are doing it correctly if Intellisense suggests an EventFunction for you. These are called subscribers
 		gui.OnClick += HandleGuiOnClick;
 		gui.OnToggle += HandleGuiOnToggle;
+		gui.OnScroll += HandleGuiOnScroll;
 		
 		//Step 1. Create your guiobjects here. 
 		gui.CreateGUIObject("MyToggle","Text",new Rect(20.0f,20.0f,100.0f,100.0f),GUIType.Toggle,"button",false);
 		gui.CreateGUIObject("Hello","Hi",new Rect(170.0f,50.0f,200.0f,300.0f),GUIType.Label,"box");
 		gui.CreateGUIObject("lolcat","",new Rect(300.0f,200.0f,100.0f,200.0f),GUIType.Button,"");
+		gui.CreateGUIObject("Slider","",new Rect(480.0f,20.0f,100.0f,20.0f),GUIType.Slider,"button",false,0.0f,100.0f,5.0f);
 		
 		//Step 2.
 		//Connect tags together based on the origin the tag up, left, right, and down from the origin. 
 		//I would suggest mapping out the scroll controls to get an idea of how you should connect them using this function
-		gui.connect("MyToggle","","","Hello","lolcat");
-		gui.connect("Hello","","","lolcat","MyToggle");
-		gui.connect("lolcat","","","MyToggle","Hello");
+		gui.connect("MyToggle","","","Slider","Hello");
+		gui.connect("Hello","","","MyToggle","lolcat");
+		gui.connect("lolcat","","","Hello","Slider");
+		gui.connect("Slider","","","lolcat","MyToggle");
 		
 		//Step 3. Assign the default tag. This tag will be the first to have a gui box selection box and the selection.
-		gui.pointer = "MyToggle";
+		gui.pointer = "Slider";
 	}
 
 	//If you created a subscriber (event handler  on line 16 and 17) these events should pop up. 
 	//name = tag of the GUIObject
 	//state = Only for toggles when they change state (on/off)
+	void HandleGuiOnScroll (object sender, ButtonName e)
+	{
+		print (e.name + " has scrolled! New value: " + e.amount);
+	}
+	
 	void HandleGuiOnToggle (object sender, ButtonName e)
 	{
 		print (e.name +" got toggled! " + e.state);
