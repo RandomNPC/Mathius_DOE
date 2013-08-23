16e473646ff299906e5fd8aa0be019ae68036f79
RandomNPC <bluedoctor7@aol.com>
2013-08-22 20:03:13 -0700

    Documentation for GUIManager updated
    More detailed explanation for the updated GUIManager

--- Mathius_Final/Assets/Components/GUIs/GUIManager/GUIManager.cs
+++ Mathius_Final/Assets/Components/GUIs/GUIManager/GUIManager.cs
@@ -12,6 +12,7 @@ public enum GUIType{
 	Slider
 }
 
+//Direction in which the perceptual/keyboard points to. Used for selecting guis. See line 110 for more info (swipe function)
 public enum Direction{
 	Left,
 	Right,
@@ -20,15 +21,14 @@ public enum Direction{
 }
 
 public class GUIManager{
-	
-	public event EventHandler<ButtonName> OnClick;
-	public event EventHandler<ButtonName> OnToggle;
-	public event EventHandler<ButtonName> OnScroll;
+	//Event handlers to handle the event functions. Important to implement! You will get a null ref exception if you dont
+	public event EventHandler<ButtonName> OnClick; //Implement if there is a button
+	public event EventHandler<ButtonName> OnToggle; //Implement if there is a toggle
+	public event EventHandler<ButtonName> OnScroll; //Implement if there is a slider
 	private GUISkin skin;
-	private Dictionary<string,GUIProperties> GUIObjects;
-	
-	private Dictionary<string,DirectionScroller> scrollmap;
-	public string pointer{get; set;}
+	private Dictionary<string,GUIProperties> GUIObjects; //Mapping each GUI Object via key value pair system
+	private Dictionary<string,DirectionScroller> scrollmap; //Mapping each scroll with gestures via key/value pair
+	public string pointer{get; set;} //pointer to the current tag. This is used for the red focus box. Required to set after adding all of the GUIObjects.
 
 	private Texture2D texture;
 	
@@ -40,21 +40,34 @@ public class GUIManager{
 		scrollmap = new Dictionary<string, DirectionScroller>();
 		scrollmap.Clear ();
 		
-		texture = new Texture2D(1,1);
+		texture = new Texture2D(1,1); //texture set to red here.
 		texture.SetPixel(0,0,Color.red);
 		texture.Apply();
 	}
 	
+	
+	//Parameters:
+	//tag: Identify each GUI object individually. Any GUIObject data that share the same tag will be overidden by the second
+	//name: GUI Object that has a name will be labeled
+	//position: Rectangular coordinates in order to draw out the GUI Object
+	//type: The type of GUI to render see lines 9-12 of this code of the available options
+	//style: using GUI.skin.getStyle(style). Provide the string of the style and if exists, will skin the GUI Object
+	//check: default setting of the checkbox when the UI interface gets opened. This parameter only applies to toggles.
+	//min: minimum value for slider
+	//max: maximum value for slider
+	//defaultVal: default value for the slider at the start
 	public void CreateGUIObject(string tag,string name, Rect position, GUIType type, string style, bool check=false, float min=0.0f, float max=0.0f, float defaultVal=0.0f){
 		GUIObjects.Add(tag,new GUIProperties(name,position,type,style,check,min,max,defaultVal));
 	}
 			
 	public void RenderGUIObjects(GUIManager gui){
-		if(gui.Equals(null))return;
+		if(gui.Equals(null))return;//I require checking the instance to avoid null ref exception errors
 		
-		GUI.skin.box.normal.background = texture;
-		showSelection(pointer,6.0f);
+		GUI.skin.box.normal.background = texture; //applying red texture here for box
+		showSelection(pointer,6.0f);//Displays a red box over the current selection
 		
+		
+		//Go thru each GUIObject to render and see if they have been pressed, where appropriate
 		foreach(KeyValuePair<string,GUIProperties> entry in GUIObjects){
 			
 			switch(entry.Value.type){
@@ -75,7 +88,6 @@ public class GUIManager{
 				case GUIType.Slider:
 					entry.Value.defaultVal = GUI.HorizontalSlider(new Rect(entry.Value.rect),entry.Value.defaultVal,entry.Value.min,entry.Value.max);
 					if(GUI.changed) OnScroll(this,new ButtonName(entry.Key,entry.Value.defaultVal));
-					
 					break;
 				default:
 					break;
@@ -83,6 +95,7 @@ public class GUIManager{
 		}
 	}
 	
+	//Selects the option, simulating a button click. This is required for perceptual coding
 	public void selectOption(string tag){
 		switch(GUIObjects[tag].type){
 			case GUIType.Button:
@@ -102,6 +115,13 @@ public class GUIManager{
 		}
 	}
 	
+	
+	// Map out each of the options
+	//------------up-------
+	//----left<-parent->right
+	//-----------down------
+	//If it is a gui slider DO NOT have labels for left and right, keep those at ""
+	//If there is no mapping to any direction, the focus will stay at the parent
 	public void connect(string parent, string left, string right, string up, string down){
 		scrollmap.Add(parent,new DirectionScroller(left,right,up,down));
 	}
@@ -117,7 +137,7 @@ public class GUIManager{
 						else if(prop.defaultVal < prop.min) prop.defaultVal = prop.min;
 						break;
 					}
-					prop.defaultVal -= 1.0f;
+					prop.defaultVal -= 1.0f;//Increment value can be changed
 				}
 		
 				break;
@@ -130,7 +150,7 @@ public class GUIManager{
 						else if(prop.defaultVal < prop.min) prop.defaultVal = prop.min;
 						break;
 					}
-					prop.defaultVal += 1.0f;
+					prop.defaultVal += 1.0f;//Increment value can be changed
 				}
 				break;
 			case Direction.Up:
@@ -144,6 +164,7 @@ public class GUIManager{
 		}
 	}
 	
+	//This renders the red GUI box around selected GUI Objects
 	private void showSelection(string tag, float delta){
 		Rect pos = GUIObjects[tag].rect;
 		GUI.Box(new Rect(pos.xMin-delta,pos.yMin-delta,pos.width+2*delta,delta),GUIContent.none); //1
