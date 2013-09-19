4ba0b8b18bca3532a029fc5065df3f7bdad35b28
RandomNPC <bluedoctor7@aol.com>
2013-09-18 22:49:10 -0700

    Spike and Shield
    Implements Issue #17
    
    Shields will protect Mathius from spikes and will take the hit instead
    of Mathius.

--- Mathius_Final/Assets/Components/Alien/Powerup Drops/Spike/Spike.cs
+++ Mathius_Final/Assets/Components/Alien/Powerup Drops/Spike/Spike.cs
@@ -5,12 +5,20 @@ public class Spike : MonoBehaviour {
 	
 	void Start () {
 		
-		Mathius mHelper = MasterController.BRAIN.m();
-		SoundManager.SOUNDS.playSound(SoundManager.PU_SPIKE,CameraCollider.MATHIUS_EARTH_CAM);
-		gameObject.GetComponent<Player>().destroy_mathius();
-		mHelper.set_lives(mHelper.get_lives()-1);
-		Vector3 cam = GameObject.Find("MathiusEarthCam").transform.position;
-		mHelper.spawn_mathius(cam.x,cam.y+15.0f,cam.z+100.0f);
+		GameObject shield = GameObject.Find("Mathius-Shield");
+		
+		if(shield.Equals(null)){
+			Mathius mHelper = MasterController.BRAIN.m();
+			SoundManager.SOUNDS.playSound(SoundManager.PU_SPIKE,CameraCollider.MATHIUS_EARTH_CAM);
+			gameObject.GetComponent<Player>().destroy_mathius();
+			mHelper.set_lives(mHelper.get_lives()-1);
+			Vector3 cam = GameObject.Find("MathiusEarthCam").transform.position;
+			mHelper.spawn_mathius(cam.x,cam.y+15.0f,cam.z+100.0f);
+		}else{
+			Destroy(shield);
+		}
+		
+
 	}
 
 }
